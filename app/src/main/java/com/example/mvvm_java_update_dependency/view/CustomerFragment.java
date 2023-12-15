d6e5287a74cc96636b9e014b3cbbb996532cea14
Zakaria <zakaria112203@gmail.com>
2023-12-15 23:46:42 +0600

    - MVVM - MutableLivedata - RxJava - Room Database - Retrofit, Api Call - Local Restore data - ViewBinding, BuildConfig - Hilt Added Day4

--- app/src/main/java/com/example/mvvm_java_update_dependency/view/CustomerFragment.java
+++ app/src/main/java/com/example/mvvm_java_update_dependency/view/CustomerFragment.java
@@ -1,29 +1,29 @@
-//package com.example.mvvm_java_update_dependency.view;
-//
-//import android.os.Bundle;
-//
-//import androidx.fragment.app.Fragment;
-//
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//
-//import com.example.mvvm_java_update_dependency.R;
-//
-//import dagger.hilt.android.AndroidEntryPoint;
-//
-//@AndroidEntryPoint
-//public class CustomerFragment extends Fragment {
-//
-//    public CustomerFragment() {
-//        // Required empty public constructor
-//    }
-//
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_customer, container, false);
-//    }
-//}
+package com.example.mvvm_java_update_dependency.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mvvm_java_update_dependency.R;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class CustomerFragment extends Fragment {
+
+    public CustomerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_customer, container, false);
+    }
+}
