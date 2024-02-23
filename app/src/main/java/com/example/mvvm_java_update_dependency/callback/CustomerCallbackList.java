01547e9fddfb3934bdbab417da7d2ee3aa75c50f
Zakaria <zakaria112203@gmail.com>
2024-02-23 22:09:28 +0600

    - MVVM - MutableLivedata - RxJava - Room Database - Retrofit, Api Call - Local Restore data - ViewBinding, BuildConfig - Hilt Added Day11 - Room Query update2

--- app/src/main/java/com/example/mvvm_java_update_dependency/callback/CustomerCallbackList.java
+++ app/src/main/java/com/example/mvvm_java_update_dependency/callback/CustomerCallbackList.java
@@ -5,6 +5,6 @@ import com.example.mvvm_java_update_dependency.model.Customer;
 import java.util.List;
 
 public interface CustomerCallbackList {
-    void onCustomer(List<Customer> customerList);//get customer
+    void onCustomer(List<Customer> customerList);
     void onCustomerError(String error); //got error from customer
 }
